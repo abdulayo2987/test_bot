aa804fd995ede1aecbb2ce7d928397a8e92b2c03
abdulayo2987 <136240146+abdulayo2987@users.noreply.github.com>
2024-12-06 15:53:16 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 # imports
 import os
+import sqlite3
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
-from responses import get_response
+from moderation import warn_user
 
-# get token
+#Get and set token from .env file so it can only be accessed by me
 load_dotenv()
 TOKEN = str(os.getenv('DISCORD_TOKEN'))
-intents: Intents = Intents.default()
-intents.message_content = True
-client: Client = Client(intents=intents)
 
+#Gives the Bot permissions so it can read/write messages
+intents = Intents.default()
+intents.message_content = True # NOQA
+client = Client(intents=intents)
 
 async def send_message(message: Message, user_message: str) -> None:
     if not user_message:
@@ -18,33 +20,23 @@ async def send_message(message: Message, user_message: str) -> None:
         return
     if is_private := user_message[0] == '?':
         user_message = user_message[1:]
-    try:
-        response: str = get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
-
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
+    if message.author == client.user:
+        return
 
 @client.event
 async def on_message(message: Message) -> None:
-    if message.author == client.user:
-        return
-    username: str = str(message.author)
-    user_message: str = message.content
-    channel: str = str(message.channel)
+    username = str(message.author)
+    user_message = message.content
+    channel = str(message.channel)
     print(f'[{channel}] {username}: "{user_message}"')
     await send_message(message, user_message)
 
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
 
 def main() -> None:
     client.run(str(TOKEN))
 
-
 if __name__ == '__main__':
     main()
-
---
+++ moderation.py
@@ -0,0 +1,10 @@
+
+def linked_list(self, data):
+    self.data = data
+    self.next = None
+
+def warn_user(user_inputs: str)-> str:
+    banned_phrase = user_inputs
+    if banned_phrase == 'dsfd':
+        return
+
